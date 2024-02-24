cf286e9740065309af3f1feca2757542a17a6b13
Khanzadi Wazir Ali <160390882+khanzadigithubid@users.noreply.github.com>
2024-02-24 13:58:40 +0500

    Qno23 Ts/Js/Tsconfig and node js of 45 Assignments
    Qno23 Ts/Js/Tsconfig and node js of 45 Assignments

---
+++ Qno23.js
@@ -0,0 +1,37 @@
+"use strict";
+/* 23. Conditional Tests: Write a series of conditional tests.
+ Print a statement describing each test and your prediction for
+ the results of each test. Your code should look something like
+ this:
+ let car = 'subaru';
+// console.log("Is car == 'subaru'? I predict True.")
+// console.log(car == 'subaru')
+// • Look closely at your results, and make sure you understand
+why each line evaluates to True or False.
+// • Create at least 10 tests. Have at least 5 tests evaluate to
+ True and another 5 tests evaluate to False.*/
+let car = 'subaru';
+/*console.log (Is car == 'subaru'? I predict True otherwise I
+predict False).*/
+console.log("Test no 1:", car == 'subaru');
+console.log("Test no 2:", car == 'civic');
+let num = 28;
+/*console.log (Is num === 23 ? I predict True otherwise I
+predict False).*/
+console.log("Test no 3", num === 28);
+console.log("Test no 4", num === '28');
+let temperature = 75;
+/*console.log (Is temperature > 75 ? I predict True otherwise I
+ predict False).*/
+console.log("Test no 5:", temperature > 70);
+console.log("Test no 6:", temperature > 100);
+let programmingLanguages = ['JavaScript', 'Python', 'Java'];
+/*console.log (Is programmingLanguages === ['JavaScript',
+'Python', 'Java'] ? I predict True otherwise I predict False).*/
+console.log("Test no 7:", programmingLanguages.length === 3);
+console.log("Test no 8:", programmingLanguages.length === 5);
+let coffeeType = 'latte';
+/*console.log (Is  let coffeeType === 'latte' ? I predict True
+ otherwise I predict False).*/
+console.log("Test no 9:", coffeeType === 'latte');
+console.log("Test no 10:", coffeeType === 'cappuccino');
